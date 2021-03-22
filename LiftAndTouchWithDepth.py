19597369a0d5ca5b58dd2cd3e6d6b943e7a09363
Rakshit <rakki22k@gmail.com>
2021-03-22 17:14:04 +0530

    add utils.py file
    Removed the redundant code by moving repeated function calls to utils.py file

--- LiftAndTouchWithDepth.py
+++ LiftAndTouchWithDepth.py
@@ -2,6 +2,7 @@ import cv2
 
 import mediapipe as mp
 import numpy as np
+from utils import *
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -23,54 +24,9 @@ def is_touch(list_avg, base_depth, threshold):
         return True
     else:
         return False
-        
 
-def find_sum(list_points,k, axis):
-    list_axis = [sub[axis] for sub in list_points]
-    
-    
-    return sum(list_axis[-k:])
-
-def add_coordinates(list_points, hand_landmarks):
-    
-    point = []
-    point.append(int(hand_landmarks.landmark[8].x*800))
-    point.append(int(hand_landmarks.landmark[8].y*800))
-    point.append(int(hand_landmarks.landmark[8].z*512))
-    
-    list_points.append(point)
-    
-    return list_points
-
-def find_average(list_points, k):
-    avg_x = find_sum(list_points, k, axis = 0)/k
-    avg_y = find_sum(list_points, k, axis = 1)/k
-    avg_z = find_sum(list_points, k, axis = 2)/k
-    return avg_x,avg_y,avg_z
-
-def add_to_average_list(list_avg, avg_x, avg_y,avg_z):
-    
-    list_avg.append([int(avg_x),int(avg_y),int(avg_z)])
-    
-    return list_avg
-
-def draw_line(canvas,list_avg,color,thickness):
-    
-    # get the current coordinates as endpoint
-    end_point = (list_avg[-1][0], list_avg[-1][1])
-    # get the previous coordinates as the start point
-    start_point = (list_avg[-2][0], list_avg[-2][1])
-    # draw a line joining these points
-    cv2.line(canvas,start_point, end_point,color, thickness)
     
     return canvas
-
-def draw_circle(cursor, list_avg, radius, color,thickness):
-    # get the current coordinates as endpoint
-    end_point = (list_avg[-1][0], list_avg[-1][1])
-    cv2.circle(cursor,end_point,radius,color,thickness)
-    
-    return cursor
 
 def draw(canvas, threshold,color, radius, thickness, k ):
     cap = cv2.VideoCapture(0)
